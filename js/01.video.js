aae64386dc8eaee95bfe15de03cb8579deb567f1
booldook <booldook@gmail.com>
2021-04-16 11:05:29 +0900

    push

--- js/01.video.js
+++ js/01.video.js
@@ -1,1 +1,12 @@
-$('#youtubeBg').YTPlayer();
+var options = {
+	videoURL:'EGGe07oYRkU',
+	containment:'.video-wrapper',
+	autoPlay:true, 
+	mute:false, 
+	startAt:0,
+	opacity:1, 
+	loop:false, 
+	ratio:"4/3", 
+	addRaster:true
+}
+$('#youtubeBg').YTPlayer(options);
