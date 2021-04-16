1391b0dac154336d4e7b44ee834ef1e8e8634046
booldook <booldook@gmail.com>
2021-04-16 11:18:30 +0900

    ...

--- js/01.video.js
+++ js/01.video.js
@@ -1,12 +1,13 @@
 var options = {
-	videoURL:'EGGe07oYRkU',
-	containment:'.video-wrapper',
-	autoPlay:true, 
-	mute:false, 
-	startAt:0,
-	opacity:1, 
-	loop:false, 
-	ratio:"4/3", 
-	addRaster:true
+	videoURL: 'EGGe07oYRkU',
+	containment: '.video-wrapper',
+	autoPlay: true, 
+	mute: true, 
+	startAt: 0,
+	opacity: .5, 
+	loop: true, 
+	ratio: '4/3', 
+	addRaster: true,
+	showControls: false
 }
 $('#youtubeBg').YTPlayer(options);
