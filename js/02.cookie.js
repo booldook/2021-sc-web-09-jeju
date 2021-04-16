37d099bc43286e7da203da95169116b1b2f00704
booldook <booldook@gmail.com>
2021-04-16 14:08:11 +0900

    modal-show/hide

---
+++ js/02.cookie.js
@@ -0,0 +1,24 @@
+/*************** 글로벌 설정 *****************/
+init()
+
+/*************** 사용자 함수 *****************/
+function init() {
+	setTimeout(onModalShow, 2000)
+}
+
+/*************** 이벤트 등록 *****************/
+$('.bt-close').click(onModalHide)
+
+
+/*************** 이벤트 콜백 *****************/
+function onModalShow() {
+	$('.modal-wrapper').show()
+	$('.modal-wrapper').css('background-color')
+	$('.modal-wrapper').css('background-color', 'rgba(0, 0, 0, 0.8)')
+	$('.modal-wrapper .modal-wrap').css('transform')
+	$('.modal-wrapper .modal-wrap').css('transform', 'translate(-50%, -50%)')
+}
+
+function onModalHide() {
+	$('.modal-wrapper').hide()
+}
