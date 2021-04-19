b717014a91616ffe063df56727b15716fc4a2935
booldook <booldook@gmail.com>
2021-04-19 16:28:18 +0900

    order 설명 끝

--- js/05.flex.js
+++ js/05.flex.js
@@ -1,4 +1,6 @@
 /*************** 글로벌 설정 *****************/
+var interval = setInterval(onShuffle, 4000)
+var justifyContent = ['space-between', 'space-around', 'space-evenly', 'flex-start', 'flex-end', 'center']
 
 
 /*************** 사용자 함수 *****************/
@@ -8,4 +10,14 @@
 
 
 /*************** 이벤트 콜백 *****************/
+function onShuffle() {
+	// $('.box-wrapper1 > .box')
+	// $('.box-wrapper1').children('.box')
+	$('.box-wrapper1').find('.box').each(function(i) {
+		var order = Math.floor(Math.random() * 5) + 1
+		$(this).css('order', order)
+	})
 
+	var n = Math.floor(Math.random() * 6)
+	$('.box-wrapper1').css('justify-content', justifyContent[n])
+}
