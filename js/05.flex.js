f24075b3982ba1e88b5904a216e22f6bf24765da
booldook <booldook@gmail.com>
2021-04-19 16:49:27 +0900

    flex 설명 끝!

--- js/05.flex.js
+++ js/05.flex.js
@@ -1,5 +1,5 @@
 /*************** 글로벌 설정 *****************/
-var interval = setInterval(onShuffle, 4000)
+// var interval = setInterval(onShuffle, 4000)
 var justifyContent = ['space-between', 'space-around', 'space-evenly', 'flex-start', 'flex-end', 'center']
 
 
@@ -7,6 +7,7 @@ var justifyContent = ['space-between', 'space-around', 'space-evenly', 'flex-sta
 
 
 /*************** 이벤트 등록 *****************/
+$('.bt-grow').click(onGrow)
 
 
 /*************** 이벤트 콜백 *****************/
@@ -20,4 +21,11 @@ function onShuffle() {
 
 	var n = Math.floor(Math.random() * 6)
 	$('.box-wrapper1').css('justify-content', justifyContent[n])
+}
+
+function onGrow() {
+	var $box = $('.box-wrapper2').children('.box')
+	var n = Math.floor(Math.random() * $box.length)
+	$box.css('flex-grow', 0)
+	$box.eq(n).css('flex-grow', 1)
 }
