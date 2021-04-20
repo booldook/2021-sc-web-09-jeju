95e2443f148c462850bfc235f39433a9f49b0789
booldook <booldook@gmail.com>
2021-04-20 12:04:44 +0900

    notice-wrapper cookie 적용

--- js/index.js
+++ js/index.js
@@ -3,30 +3,39 @@ init()
 
 /*************** 사용자 함수 *****************/
 function init() {
-	$('.header-wrapper').find('.notice-content').hide()
-	$('.header-wrapper').find('.bt-hide').hide()
+	if($.cookie('hideNotice') === 'Y') onCloseNotice()
+	else {
+		$('.header-wrapper').find('.notice-content').hide()
+		$('.header-wrapper').find('.bt-hide').hide()
+	}
 }
 
 /*************** 이벤트 등록 *****************/
-$('.header-wrapper .bt-show').click(onNoticeShow)
-$('.header-wrapper .bt-hide').click(onNoticeHide)
-$('.header-wrapper .bt-close').click(onNoticeClose)
+$('.header-wrapper .bt-show').click(onShowNotice)
+$('.header-wrapper .bt-hide').click(onHideNotice)
+$('.header-wrapper .bt-close').click(onCloseNotice)
+$('.header-wrapper .bt-today').click(onHideTodayNotice)
 
 
 /*************** 이벤트 콜백 *****************/
-function onNoticeShow() {
+function onShowNotice() {
 	$('.header-wrapper').find('.bt-show').hide()
 	$('.header-wrapper').find('.bt-hide').show()
 	$('.header-wrapper').find('.notice-content').show()
 }
 
-function onNoticeHide() {
+function onHideNotice() {
 	$('.header-wrapper').find('.bt-show').show()
 	$('.header-wrapper').find('.bt-hide').hide()
 	$('.header-wrapper').find('.notice-content').hide()
 }
 
-function onNoticeClose() {
+function onCloseNotice() {
 	$('.header-wrapper').find('.notice-wrapper').hide()
+}
+
+function onHideTodayNotice() {
+	$.cookie('hideNotice', 'Y', { expires: 1, path: '/' })
+	onCloseNotice()
 }
 
