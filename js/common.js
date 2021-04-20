708e29f12ce90e956fd6fb6311e062ec0353fa17
booldook <booldook@gmail.com>
2021-04-20 14:06:36 +0900

    1. top-wrapper 추가 및 수정 2. link-lang 작업 완료

---
+++ js/common.js
@@ -0,0 +1,56 @@
+/*************** 글로벌 설정 *****************/
+initCommon()
+
+/*************** 사용자 함수 *****************/
+function initCommon() {
+	if($.cookie('hideNotice') === 'Y') onCloseNotice()
+	else {
+		$('.header-wrapper').find('.notice-content').hide()
+		$('.header-wrapper').find('.bt-hide').hide()
+	}
+}
+
+/*************** 이벤트 등록 *****************/
+$('.header-wrapper .bt-show').click(onShowNotice)
+$('.header-wrapper .bt-hide').click(onHideNotice)
+$('.header-wrapper .bt-close').click(onCloseNotice)
+$('.header-wrapper .bt-today').click(onHideTodayNotice)
+$('.header-wrapper .link-lang').click(onToggleLang)
+$('.header-wrapper .link-lang').mouseenter(onShowLang)
+$('.header-wrapper .link-lang').mouseleave(onHideLang)
+
+
+/*************** 이벤트 콜백 *****************/
+function onShowNotice() {
+	$('.header-wrapper').find('.bt-show').hide()
+	$('.header-wrapper').find('.bt-hide').show()
+	$('.header-wrapper').find('.notice-content').show()
+}
+
+function onHideNotice() {
+	$('.header-wrapper').find('.bt-show').show()
+	$('.header-wrapper').find('.bt-hide').hide()
+	$('.header-wrapper').find('.notice-content').hide()
+}
+
+function onCloseNotice() {
+	$('.header-wrapper').find('.notice-wrapper').hide()
+}
+
+function onHideTodayNotice() {
+	$.cookie('hideNotice', 'Y', { expires: 1, path: '/' })
+	onCloseNotice()
+}
+
+function onToggleLang() {
+	$('.header-wrapper .link-lang .hover').toggle()
+}
+
+function onShowLang() {
+	$('.header-wrapper .link-lang .hover').show()
+}
+
+function onHideLang() {
+	$('.header-wrapper .link-lang .hover').hide()
+}
+
--- js/index.js
+++ js/index.js
@@ -1,41 +1,11 @@
 /*************** 글로벌 설정 *****************/
-init()
+
 
 /*************** 사용자 함수 *****************/
-function init() {
-	if($.cookie('hideNotice') === 'Y') onCloseNotice()
-	else {
-		$('.header-wrapper').find('.notice-content').hide()
-		$('.header-wrapper').find('.bt-hide').hide()
-	}
-}
+
 
 /*************** 이벤트 등록 *****************/
-$('.header-wrapper .bt-show').click(onShowNotice)
-$('.header-wrapper .bt-hide').click(onHideNotice)
-$('.header-wrapper .bt-close').click(onCloseNotice)
-$('.header-wrapper .bt-today').click(onHideTodayNotice)
 
 
 /*************** 이벤트 콜백 *****************/
-function onShowNotice() {
-	$('.header-wrapper').find('.bt-show').hide()
-	$('.header-wrapper').find('.bt-hide').show()
-	$('.header-wrapper').find('.notice-content').show()
-}
-
-function onHideNotice() {
-	$('.header-wrapper').find('.bt-show').show()
-	$('.header-wrapper').find('.bt-hide').hide()
-	$('.header-wrapper').find('.notice-content').hide()
-}
-
-function onCloseNotice() {
-	$('.header-wrapper').find('.notice-wrapper').hide()
-}
-
-function onHideTodayNotice() {
-	$.cookie('hideNotice', 'Y', { expires: 1, path: '/' })
-	onCloseNotice()
-}
 
