c1b3745db35f24d02d51c1f8f2d19fa0c1d9e20c
booldook <booldook@gmail.com>
2021-04-20 11:49:47 +0900

    notice-wrapper show/hide 구현

--- js/index.js
+++ js/index.js
@@ -1,11 +1,32 @@
 /*************** 글로벌 설정 *****************/
-
+init()
 
 /*************** 사용자 함수 *****************/
-
+function init() {
+	$('.header-wrapper').find('.notice-content').hide()
+	$('.header-wrapper').find('.bt-hide').hide()
+}
 
 /*************** 이벤트 등록 *****************/
+$('.header-wrapper .bt-show').click(onNoticeShow)
+$('.header-wrapper .bt-hide').click(onNoticeHide)
+$('.header-wrapper .bt-close').click(onNoticeClose)
 
 
 /*************** 이벤트 콜백 *****************/
+function onNoticeShow() {
+	$('.header-wrapper').find('.bt-show').hide()
+	$('.header-wrapper').find('.bt-hide').show()
+	$('.header-wrapper').find('.notice-content').show()
+}
+
+function onNoticeHide() {
+	$('.header-wrapper').find('.bt-show').show()
+	$('.header-wrapper').find('.bt-hide').hide()
+	$('.header-wrapper').find('.notice-content').hide()
+}
+
+function onNoticeClose() {
+	$('.header-wrapper').find('.notice-wrapper').hide()
+}
 
