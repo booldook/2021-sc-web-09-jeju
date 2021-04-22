948af97a2e3a80aae6755b8ba92775b0d1544cc8
booldook <booldook@gmail.com>
2021-04-22 14:15:28 +0900

    메인 배너 구현중

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ $(function() {
 	/*************** 글로벌 설정 *****************/
 	var $wrapper = $('.main-wrapper')
 	var $slide = $('.main-wrapper .slide')
+	var video = $('.main-wrapper .video')[0]
 	var len = $slide.length
 	var lastIdx = len - 1
 	var depth = 2
@@ -19,12 +20,12 @@ $(function() {
 	var speed = 500
 	var interval
 	init()
-
-
+	
+	
 	/*************** 사용자 함수 *****************/
 	function init() {
 		$slide.eq(idx).css('z-index', depth++)
-		interval = setInterval(onAni, gap)
+		onAni()
 	}
 
 
@@ -35,8 +36,12 @@ $(function() {
 	/* if(idx == lastIdx) mainIdx = 0
 	else idx = idx + 1 */
 	function onAni() {
+		if($slide.eq(idx).hasClass('is-video')) {
+			
+		}
 		idx = (idx == lastIdx) ? 0 : idx + 1
-		$slide.eq(idx).css('z-index', depth++)
+		$slide.eq(idx).css({'z-index': depth++, 'left': '100%'})
+		$slide.eq(idx).stop().animate({'left': 0}, speed)
 	}
 
 })
