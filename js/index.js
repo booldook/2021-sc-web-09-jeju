231e9f6f2c4ee35d136b14a441693636a8a2d6da
booldook <booldook@gmail.com>
2021-04-22 12:21:44 +0900

    mainWrapper slide 구현중

--- js/index.js
+++ js/index.js
@@ -5,13 +5,32 @@
 
 
 /*************** 글로벌 설정 *****************/
+var $mainWrapper = $('.main-wrapper')
+var $mainSlide = $('.main-wrapper .slide')
+var mainLen = $mainSlide.length
+var mainLastIdx = mainLen - 1
+var mainDepth = 2
+var mainIdx = 0
+var mainGap = 3000
+var mainSpeed = 500
+var mainInterval
+initMain()
 
 
 /*************** 사용자 함수 *****************/
+function initMain() {
+	$mainSlide.eq(mainIdx).css('z-index', mainDepth++)
+	mainInterval = setInterval(onMainAni, mainGap)
+}
 
 
 /*************** 이벤트 등록 *****************/
 
 
 /*************** 이벤트 콜백 *****************/
-
+/* if(mainIdx == mainLastIdx) mainIdx = 0
+else mainIdx = mainIdx + 1 */
+function onMainAni() {
+	mainIdx = (mainIdx == mainLastIdx) ? 0 : mainIdx + 1
+	$mainSlide.eq(mainIdx).css('z-index', mainDepth++)
+}
