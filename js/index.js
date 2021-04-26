8c6e1f8e4b640cbc608740cb2b504a58b99e543b
booldook <booldook@gmail.com>
2021-04-26 16:00:54 +0900

    main-wrapper의 날씨정보 만들기

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ $(function() {
 	var $slide = $('.main-wrapper .slide')
 	var $pagerSlide = $('.main-wrapper .pager-slide')
 	var video = $('.main-wrapper .video')[0]
+	var $weather = $('.main-wrapper .weather') 
 	var len = $slide.length
 	var lastIdx = len - 1
 	var depth = 2
@@ -21,8 +22,33 @@ $(function() {
 	var gap = 5000
 	var speed = 500
 	var timeout
+	var weatherURL = 'https://api.openweathermap.org/data/2.5/weather'
+	var weatherData = {
+		appid: '02efdd64bdc14b279bc91d9247db4722',
+		units: 'metric'
+	}
+	var weatherIcon = {
+		i01d: 'bi-brightness-high',
+		i01n: 'bi-brightness-high-fill',
+		i02d: 'bi-cloud-sun',
+		i02n: 'bi-cloud-sun-fill',
+		i03d: 'bi-cloud',
+		i03n: 'bi-cloud-fill',
+		i04d: 'bi-clouds',
+		i04n: 'bi-cloud-fills',
+		i09d: 'bi-cloud-rain-heavy',
+		i09n: 'bi-cloud-rain-heavy-fill',
+		i10d: 'bi-cloud-drizzle',
+		i10n: 'bi-cloud-drizzle-fill',
+		i11d: 'bi-cloud-lightning',
+		i11n: 'bi-cloud-lightning-fill',
+		i13d: 'bi-cloud-snow',
+		i13n: 'bi-cloud-snow-fill',
+		i50d: 'bi-cloud-haze',
+		i50n: 'bi-cloud-haze-fill',
+	}
 	init()
-	
+	weather()
 	
 	/*************** 사용자 함수 *****************/
 	function init() {
@@ -39,6 +65,11 @@ $(function() {
 		ani()
 	}
 
+	function weather() {
+		// 위치정보 가져오기(못 가져오면 제주도 보이기 33.485739737138786, 126.48154043372092)
+		navigator.geolocation.getCurrentPosition(onGetGeo, onErrorGeo)
+	}
+
 
 	/*************** 이벤트 등록 *****************/
 	video.addEventListener('loadeddata', onLoadedVideo)
@@ -50,6 +81,24 @@ $(function() {
 
 
 	/*************** 이벤트 콜백 *****************/
+	function onGetWeather(r) {
+		console.log(r)
+		$weather.find('.icon').addClass(weatherIcon['i'+r.weather[0].icon])
+		$weather.find('.temp').text(r.main.temp)
+	}
+
+	function onGetGeo(r) {
+		weatherData.lat = r.coords.latitude
+		weatherData.lon = r.coords.longitude
+		$.get(weatherURL, weatherData, onGetWeather)
+	}
+
+	function onErrorGeo() {
+		weatherData.lat = 33.485739737138786
+		weatherData.lon = 126.48154043372092
+		$.get(weatherURL, weatherData, onGetWeather)
+	}
+
 	function onPagerClick() {
 		idx = $(this).index()
 		onPlay('pager')
