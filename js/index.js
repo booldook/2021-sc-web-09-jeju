d6c20ac39506783ac7b6d123c194b48a6970a9e2
booldook <booldook@gmail.com>
2021-04-27 12:17:08 +0900

    dream-wrapper slide 작업중

--- js/index.js
+++ js/index.js
@@ -3,18 +3,18 @@
 // 2. 동영상은 플레이가 끝난 이후에 다음으로 넘어간다
 // 3. 배너가 나타나면 그 후에 내부요소들이 animation으로 나타난다
 
-	/* if(idx == lastIdx) mainIdx = 0
-	else idx = idx + 1 */
+/* if(idx == lastIdx) mainIdx = 0
+else idx = idx + 1 */
 
 /*************** main-wrapper *****************/
-$(function() {
+$(function () {
 
 	/*************** 글로벌 설정 *****************/
 	var $wrapper = $('.main-wrapper')
 	var $slide = $('.main-wrapper .slide')
 	var $pagerSlide = $('.main-wrapper .pager-slide')
 	var video = $('.main-wrapper .video')[0]
-	var $weather = $('.main-wrapper .weather') 
+	var $weather = $('.main-wrapper .weather')
 	var len = $slide.length
 	var lastIdx = len - 1
 	var depth = 2
@@ -49,14 +49,14 @@ $(function() {
 	}
 	init()
 	weather()
-	
+
 	/*************** 사용자 함수 *****************/
 	function init() {
-		if($.cookie('hideCookie') === 'Y') onCloseCookie()
+		if ($.cookie('hideCookie') === 'Y') onCloseCookie()
 		$slide.eq(idx).css('z-index', depth++)
 		$slide.eq(idx).addClass('active')
 
-		for(var i=0; i<len; i++) $pagerSlide.append('<i class="pager"></i>')
+		for (var i = 0; i < len; i++) $pagerSlide.append('<i class="pager"></i>')
 		$pagerSlide.find('.pager').click(onPagerClick)
 		$pagerSlide.find('.pager').eq(idx).addClass('active')
 
@@ -64,11 +64,32 @@ $(function() {
 			$('<i class="pager"></i>').appendTo($pagerSlide).click(onPagerClick).addClass((idx === i) ? 'active': '')
 		} */
 		ani()
+		slideDream()
 	}
 
 	function weather() {
 		// 위치정보 가져오기(못 가져오면 제주도 보이기 33.485739737138786, 126.48154043372092)
 		navigator.geolocation.getCurrentPosition(onGetGeo, onErrorGeo)
+	}
+
+	function slideDream() {
+		var swiper = new Swiper('.dream-wrapper .swiper-container', {
+			pagination: {
+				el: '.dream-wrapper .swiper-pagination',
+			},
+			slidesPerView: 1,
+			spaceBetween: 10,
+			breakpoints: {
+				576: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 30,
+				}
+			}
+		});
 	}
 
 
@@ -84,13 +105,16 @@ $(function() {
 
 	/*************** 이벤트 콜백 *****************/
 	function onCloseTodayCookie() {
-		$.cookie('hideCookie', 'Y', { expires: 1, path: '/' })
+		$.cookie('hideCookie', 'Y', {
+			expires: 1,
+			path: '/'
+		})
 		onCloseCookie()
 	}
 
 	function onGetWeather(r) {
 		console.log(r)
-		$weather.find('.icon').addClass(weatherIcon['i'+r.weather[0].icon])
+		$weather.find('.icon').addClass(weatherIcon['i' + r.weather[0].icon])
 		$weather.find('.temp').text(r.main.temp)
 		$weather.find('.date').text(moment(r.dt * 1000).format('YYYY. M. D. ddd'))
 		$weather.find('.time > span').text(moment(r.dt * 1000).format('hh:mm'))
@@ -127,15 +151,15 @@ $(function() {
 	}
 
 	function onLoadedVideo() {
-		if(video.readyState >= 2) {
+		if (video.readyState >= 2) {
 			video.playbackRate = 4.0
 		}
 	}
-	
+
 	function ani() {
 		$(this).addClass('active')
 		video.currentTime = 0
-		if($slide.eq(idx).hasClass('is-video')) video.play()
+		if ($slide.eq(idx).hasClass('is-video')) video.play()
 		else {
 			clearTimeout(timeout)
 			timeout = setTimeout(onPlay, gap)
@@ -143,12 +167,17 @@ $(function() {
 	}
 
 	function onPlay(e) {
-		if(e !== 'pager') idx = (idx == lastIdx) ? 0 : idx + 1
+		if (e !== 'pager') idx = (idx == lastIdx) ? 0 : idx + 1
 		$pagerSlide.find('.pager').removeClass('active')
 		$pagerSlide.find('.pager').eq(idx).addClass('active')
-		$slide.eq(idx).css({'z-index': depth++, 'left': '100%'})
+		$slide.eq(idx).css({
+			'z-index': depth++,
+			'left': '100%'
+		})
 		$slide.removeClass('active')
-		$slide.eq(idx).stop().animate({'left': 0}, speed, ani)
+		$slide.eq(idx).stop().animate({
+			'left': 0
+		}, speed, ani)
 	}
 
 })
