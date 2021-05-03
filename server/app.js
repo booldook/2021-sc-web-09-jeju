545b5b062a85ad820c8196fb0dddf2db4f995387
booldook <booldook@gmail.com>
2021-05-03 11:05:01 +0900

    12.form 배우는 중

--- server/app.js
+++ server/app.js
@@ -5,6 +5,17 @@ app.listen(3000, function() {
 	console.log('http://127.0.0.1:3000')
 })
 
+app.use( express.json() )
+app.use( express.urlencoded({ extended: false }) )
+
 app.get('/', function(req, res) {
-	res.send('<h1>서버가 시작되었습니다.</h1>')
+	res.send('<h1>서버 구동</h1>')
+})
+
+app.get('/test', function(req, res) {
+	res.json(req.query)
+})
+
+app.post('/test2', function(req, res) {
+	res.json(req.body)
 })
